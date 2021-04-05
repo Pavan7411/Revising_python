59fbabcdd1d4a6358b35ae2043dc0dafa4b43eaf
Pavan7411 <48238110+Pavan7411@users.noreply.github.com>
2021-04-05 16:48:56 +0530

    Add files via upload

---
+++ more_square_forloop.py
@@ -0,0 +1,17 @@
+import turtle
+turtle.shape("turtle")
+
+for i in range(0,36): 
+
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.left(90)
+
+    turtle.left(10)
+    i+=1
+
